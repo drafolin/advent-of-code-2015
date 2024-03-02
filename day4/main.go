0330c7e6a0923189232daa1f2cb57e71f9db1477
Dråfølin <derg@drafolin.ch>
2024-03-02 16:02:34 +0100

    day 4 part 1

---
+++ day4/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	in, salt := "iwrupvqb", 0
+
+	for {
+		salt++
+		hash := fmt.Sprintf("%x", md5.Sum([]byte(fmt.Sprintf("%s%d", in, salt))))
+		if strings.HasPrefix(hash, "00000") {
+			break
+		}
+	}
+
+	fmt.Println("the smallest salt value is", salt)
+}
