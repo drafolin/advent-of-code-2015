f7c2cf2d60c01db9f11c704544cfc9372baf58e5
Dråfølin <derg@drafolin.ch>
2024-03-02 17:42:08 +0100

    day 6 part 1

---
+++ day6/main.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Op int64
+
+const (
+	TURN_ON Op = iota
+	TURN_OFF
+	TOGGLE
+)
+
+type Coordinate struct {
+	X, Y int
+}
+
+type Operation struct {
+	from, to Coordinate
+	op       Op
+}
+
+func main() {
+	f, err := os.ReadFile("day6/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	operations := ParseInput(string(f))
+
+	var lights [1000][1000]bool = [1000][1000]bool{}
+
+	for y := range lights {
+		lights[y] = [1000]bool{}
+		for x := range lights[y] {
+			lights[y][x] = false
+		}
+	}
+
+	for _, op := range operations {
+		for y := op.from.Y; y <= op.to.Y; y++ {
+			for x := op.from.X; x <= op.to.X; x++ {
+				switch op.op {
+				case TURN_ON:
+					lights[y][x] = true
+				case TURN_OFF:
+					lights[y][x] = false
+				case TOGGLE:
+					lights[y][x] = !lights[y][x]
+				}
+			}
+		}
+	}
+
+	total := 0
+	for _, row := range lights {
+		total += Count(row[:], true)
+	}
+
+	fmt.Println(total, "are turned on")
+}
+
+var exp = Assert(regexp.Compile("([a-z ]*) ([0-9]{1,3}),([0-9]{1,3}) through ([0-9]{1,3}),([0-9]{1,3})"))
+
+func ParseInput(f string) []Operation {
+	res := []Operation{}
+	for _, s := range strings.Split(f, "\n") {
+		matches := exp.FindStringSubmatch(s)
+		if matches == nil {
+			continue
+		}
+
+		fromX, fromY, toX, toY :=
+			Assert(strconv.Atoi(matches[2])),
+			Assert(strconv.Atoi(matches[3])),
+			Assert(strconv.Atoi(matches[4])),
+			Assert(strconv.Atoi(matches[5]))
+
+		var operation Op
+
+		switch matches[1] {
+		case "turn off":
+			operation = TURN_OFF
+		case "turn on":
+			operation = TURN_ON
+		case "toggle":
+			operation = TOGGLE
+		}
+
+		res = append(res, Operation{
+			from: Coordinate{fromX, fromY},
+			to:   Coordinate{toX, toY},
+			op:   operation,
+		})
+	}
+	return res
+}
+
+func Assert[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+func Count[T comparable](s []T, ex T) int {
+	cnt := 0
+
+	for _, v := range s {
+		if v == ex {
+			cnt++
+		}
+	}
+	return cnt
+}
