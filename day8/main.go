8671c346d5a43756146ebb7ad554871b9632cd8a
Dråfølin <derg@drafolin.ch>
2024-03-03 17:21:56 +0100

    day 8 part 1

---
+++ day8/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.ReadFile("day8/input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	total := 0
+
+	for _, line := range strings.Split(string(f), "\n") {
+		if line == "" {
+			continue
+		}
+		fmt.Println(line)
+		result, err := strconv.Unquote(line)
+
+		if err != nil {
+			panic(err)
+		}
+		total += len(line) - len(result)
+	}
+
+	fmt.Println(total)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/drafolin/advent-of-code-2015
 
 go 1.22.0
+
+require github.com/Knetic/govaluate v3.0.0+incompatible
